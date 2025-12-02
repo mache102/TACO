2456e563e13d23845ba4b5b2657a66c3b15d57d0
mache102 <bee365go@gmail.com>
2025-12-01 19:07:53 -0800

    evalscript

---
+++ eval_kodak.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+declare -a lambdas=("0.0004" "0.0008" "0.0016" "0.004" "0.009" "0.015")
+
+for lambda in "${lambdas[@]}"; do
+    python -u generate_images_using_image_cap_dataset.py --image_folder_root /home/ying/datasets/kodak/dataset/ --image_cap_dict_root ./materials/kodak_ofa.json --checkpoint ./ckpts/lambda_${lambda}.pth.tar --out kodak/lambda_${lambda}
+done
